f10c0690610001c1acebb1fa9767753c996dac48
unknown <dominikcubic@gmail.com>
2024-04-04 18:18:06 -0700

    :tada: starting project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        Shift the overall look and feel by adding these wonderful 
+        touches to furniture in your home
+
+        Ever been in a room and felt like something was missing? Perhaps 
+        it felt slightly bare and uninviting. I’ve got some simple tips 
+        to help you make any room feel complete.
+
+        Michelle Appleton
+        28 Jun 2020
+
+        Share
+    </>
+  )
+}
+
+export default App
