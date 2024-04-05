86b217315e1baa6b7fd32ad9b8eb23dba8f91f42
unknown <dominikcubic@gmail.com>
2024-04-05 14:38:12 -0700

    :heavy_bag_sign: started using svgr to modify svg images dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 import getWindowDimensions from "./utils/utils.jsx";
 import drawersImage from "./assets/images/drawers.jpg";
 import avatarImage from "./assets/images/avatar-michelle.jpg";
-import shareIcon from "./assets/images/icon-share.svg";
+
+import ShareIcon from "./assets/images/icon-share.svg?react";
+import facebookIcon from "./assets/images/icon-facebook.svg";
+import pinterestIcon from "./assets/images/icon-pinterest.svg";
+import twitterIcon from "./assets/images/icon-twitter.svg";
+
 import styles from "./App.module.css";
 
 function App() {
@@ -30,22 +35,50 @@ function App() {
           felt slightly bare and uninviting. I’ve got some simple tips to help
           you make any room feel complete.
         </p>
+      </section>
+      {!isClicked && (
         <footer className={styles.footer}>
           <div className={styles.portraitNameLayout}>
-            <img src={avatarImage} alt="Portrait image of Michelle" className={styles.avatarImage}></img>
-            <div className={styles.nameContainer}>
+            <img
+              src={avatarImage}
+              alt="Portrait image of Michelle"
+              className={styles.avatarImage}
+            ></img>
+            <div className={styles.personalInfoContainer}>
               <h2 className={styles.h2}>Michelle Appleton</h2>
               <p className={styles.dateParagraph}>28 Jun 2020</p>
             </div>
           </div>
-          <button onClick={handleClick}>
-            <img
-              src={shareIcon}
-              alt="image of a curved arrow that is meant to represent sharing"
-            ></img>
+          <button onClick={handleClick} className={styles.shareButton}>
+            <ShareIcon fill="#6E8098" alt="image of a curved arrow that is meant to represent sharing"/>
           </button>
         </footer>
-      </section>
+      )}
+      {isClicked && (
+        <footer className={`${styles.activeShareFooter} ${styles.footer}`}>
+          <div className={styles.links}>
+            <span className={styles.shareText}>Share</span>
+            <img
+              src={facebookIcon}
+              alt="Facebook icon"
+              className={styles.socialMediaButton}
+            ></img>
+            <img
+              src={twitterIcon}
+              alt="Twitter icon"
+              className={styles.socialMediaButton}
+            ></img>
+            <img
+              src={pinterestIcon}
+              alt="Pinterest icon"
+              className={styles.socialMediaButton}
+            ></img>
+          </div>
+          <button onClick={handleClick} className={`${styles.shareButton} ${styles.grayBackground}`}>
+            <ShareIcon fill="white" alt="image of a curved arrow that is meant to represent sharing"/>
+          </button>
+        </footer>
+      )}
     </main>
   );
 }
